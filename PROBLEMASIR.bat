ac247da1341d60dcc75be85c2f28d49bd7eafed9
Santiago Salas <santiago.salas.v@hotmail.de>
2014-08-27 20:37:25 -0500

    Added batch file to execute in computer with deployed but not installed MCR

---
+++ PROBLEMASIR.bat
@@ -0,0 +1,7 @@
+echo off
+echo Agregando variables de entorno del usuario
+setx path "C:\Program Files (x86)\Matlab Compiler Runtime\v716\runtime\win32;C:\Program Files (x86)\Matlab Compiler Runtime\v716\bin"
+echo Variables de entorno agregadas
+echo Comenzando programa
+echo on
+PROBLEMASIR.exe
